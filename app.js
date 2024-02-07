8310259e3a6d1396d08a4d6ec8722090def23ef6
Gabriel Amaral <gabriel2012amaral@gmail.com>
2024-02-06 21:09:42 -0300

    controle estoque

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const vm = new Vue({
     produtos: [],
     produto: false,
     carrinho: [],
+    carrinhoAtivo: false,
     mensagemAlerta: "Item adicionado",
     alertaAtivo: false,
   },
@@ -48,9 +49,11 @@ const vm = new Vue({
         behavior: "smooth",
       });
     },
-
     fecharModal({ target, currentTarget }) {
       if (target === currentTarget) this.produto = false;
+    },
+    clickForaCarrinho({ target, currentTarget }) {
+      if (target === currentTarget) this.carrinhoAtivo = false;
     },
     adicionarItem() {
       this.produto.estoque--;
